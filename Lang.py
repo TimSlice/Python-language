570d1a93d005d427cb4f38e81759edbbc96d2fce
Tim Slice <rt2monyk-12@mail.ru>
2021-10-26 19:07:05 +0300

    just fo fun

---
+++ Lang.py
@@ -0,0 +1,18 @@
+x="name"
+print(x.lower())
+print(x.title())
+print(x.upper())
+z=input("Введите значение:\n")
+y="Вы написали " + z + "!"
+print(y)
+print("I love you\n\tAnd you...")
+a=' fghddh '
+print(a)
+a=a.strip()
+print(a)
+v = "ncnmnkfgkjfkg"
+k = "jkhkdjfhgkjd"
+result = v + k
+print(result*5)
+print(len(result*5))
+print(result[4])
